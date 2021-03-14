5591e38b3e09c6c95816bbfd1397e67c72d64b2d
Harry Horsley <hslhcam2@gmail.com>
2021-03-14 16:10:30 +0000

    PEP8 and error fixing

--- robot_config.py
+++ robot_config.py
@@ -1,27 +1,28 @@
 import numpy as np
 
-class Robot():
+
+class Robot:
 
     def __init__(self, type, position):
-        self._type = type           # Type will be an integer
-        self._position = position   # X-y position
+        self._type = type  # Type will be an integer
+        self._position = position  # X-Y position
 
         # Initialise values
-        self._medical_payload = 0           # Set initial payloads to 0
+        self._medical_payload = 0  # Set initial payloads to 0
         self._life_payload = 0
         self._is_full = False
 
         # For each type of robot define capabilities
 
         # Basic type
-        if type = 0:
-            self._capacity = 10             # Payload capacity
-            self._speed = 1                 # Speed
-            self._fuelcap = 100             # Fuel capacity
-            self._fuel = self._fuelcap      # Set initial fuel as a full tank
+        if type == 0:
+            self._capacity = 10  # Payload capacity
+            self._speed = 1  # Speed
+            self._fuelcap = 100  # Fuel capacity
+            self._fuel = self._fuelcap  # Set initial fuel as a full tank
             self._weight = 10
 
-        elif type = 1:
+        elif type == 1:
             self._capacity = 10  # Payload capacity
             self._speed = 1  # Speed
             self._fuelcap = 100  # Fuel capacity
@@ -32,10 +33,10 @@ class Robot():
             raise Exception("Invalid robot type given")
 
         # Set up the fuel drain rate, the same for all types as we assume engines are the same
-        #self._drainrate = self.get_drainrate()
+        # self._drainrate = self.get_drainrate()
 
-    def node_move(self, start_node, end_node):
-        self._position = end_node.position      """Need to check that this is what Marcin has done!"""
+    # def node_move(self, start_node, end_node):
+    #     self._position = end_node.position      """Need to check that this is what Marcin has done!"""
 
     def move(self, start_position, end_position):
         self._position = end_position
@@ -48,7 +49,8 @@ class Robot():
         self._fuel -= drain_rate * distance
         # Could do some stuff here with negative fuel, but leave it for now as its too complex
 
-    def get_drainrate(self):        """ Need to change these coefficients so that it makes sense"""
+    def get_drainrate(self):
+        """ Need to change these coefficients so that it makes sense"""
         a = 1
         b = 1
         c = 1
@@ -58,13 +60,12 @@ class Robot():
         """ Should be adding these one at a time, so don't need to pass anything?"""
         if self._is_full:
             print("Robot at full capacity")
-            return False        # Returning False here so that the super code could figure out a failure?
+            return False  # Returning False here so that the super code could figure out a failure?
         else:
             self._life_payload += 1
             if (self._life_payload + self._medical_payload) == self._capacity:
                 self._is_full = True
             return True
-
 
     def add_unit_medical(self):
         if self._is_full:
@@ -79,10 +80,6 @@ class Robot():
     @property
     def is_full(self):
         return self._is_full
-
-    @property
-    def current_payload(self):
-        return self._life_payload + self._medical_payload
 
     @property
     def type(self):
@@ -109,8 +106,16 @@ class Robot():
         return self._fuelcap
 
     @property
+    def medical_payload(self):
+        return self._medical_payload
+
+    @property
+    def life_payload(self):
+        return self._life_payload
+
+    @property
     def payload(self):
-        return self._payload
+        return self._life_payload + self._medical_payload
 
     @property
     def weight(self):
