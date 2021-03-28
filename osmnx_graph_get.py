90272f6eec77d557802a3570dda8939f916d9f22
Harry Horsley <hslhcam2@gmail.com>
2021-03-28 18:39:52 +0100

    Added in a Sydney graph, but it is quite small

--- osmnx_graph_get.py
+++ osmnx_graph_get.py
@@ -1,7 +1,7 @@
 import osmnx as ox
 import pathlib
 
-map_location = "Cambridge"
+map_location = "Sydney"
 
 path = pathlib.Path(__file__).parent.absolute()
 
@@ -10,5 +10,13 @@ if map_location == "Cambridge":
     filepath = path / "Cambridge_Graph.xml"
 
     graph = ox.get_undirected(ox.graph_from_address('Cambridge, United Kingdom', network_type="drive_service"))
+
+    ox.io.save_graphml(graph, filepath=filepath)
+
+elif map_location == "Sydney":
+
+    filepath = path / "Sydney_Graph.xml"
+
+    graph = ox.get_undirected(ox.graph_from_address('Sydney, Australia', network_type="drive_service"))
 
     ox.io.save_graphml(graph, filepath=filepath)
